cb78b5b7a3ecd48b7717d9c7225560ad3f0220ce
Jan Buntenbroich <jan@buntenbroich.de>
2025-12-17 18:16:54 +0100

    h1 fertig

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -2,6 +2,37 @@ package h1;
 
 public class H1_main {
     public static void main(String[] args) {
+        PrioListe liste = new PrioListe();
 
+        Patient A = new Patient("A", 1);
+        Patient B = new Patient("B", 10);
+        Patient C = new Patient("C", 5);
+        Patient D = new Patient("D", 7);
+
+        liste.addPatient(A);
+        liste.addPatient(B);
+        liste.addPatient(C);
+
+        System.out.println("Liste nach Hinzufügen von A, B, C:");
+        printListe(liste);
+
+        liste.addPatient(D);
+        System.out.println("\nListe nach Hinzufügen von D:");
+        printListe(liste);
+
+        Patient next = liste.getNextPatient();
+        System.out.println("\nNächster Patient aufgerufen: " + next.getName());
+        System.out.println("Liste nach Aufruf des nächsten Patienten:");
+        printListe(liste);
+
+        System.out.println("\nPosition von D: " + liste.getPosition(D));
+    }
+
+    public static void printListe(PrioListe liste) {
+        for (int i = 0; i < liste.myList.size(); i++) {
+            Patient p = liste.myList.get(i);
+            System.out.println("Position " + i + ": " + p.getName() + " (Prio " + p.getPrio() + ")");
+        }
     }
 }
+
